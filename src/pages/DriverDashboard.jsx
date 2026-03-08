bd5e2e20756edf4356836b52da64d43e005cdb90
Kang Zhiwei <windstrafer@gmail.com>
2026-03-08 04:46:43 +0000

    File changes

--- src/pages/DriverDashboard.jsx
+++ src/pages/DriverDashboard.jsx
@@ -164,7 +164,7 @@ export default function DriverDashboard() {
                   onUpdateLocation={(coords) =>
                     handleUpdateLocation(transfer.id, vehicle?.id, coords)
                   }
-                  onConfirmDelivery={() => handleConfirmDelivery(transfer.id)}
+                  onConfirmDelivery={(confirmData) => handleConfirmDelivery(transfer.id, confirmData)}
                   updating={updatingTask === transfer.id}
                   confirming={confirmingTask === transfer.id}
                 />
@@ -191,7 +191,7 @@ export default function DriverDashboard() {
                   onUpdateLocation={(coords) =>
                     handleUpdateLocation(transfer.id, vehicle?.id, coords)
                   }
-                  onConfirmDelivery={() => handleConfirmDelivery(transfer.id)}
+                  onConfirmDelivery={(confirmData) => handleConfirmDelivery(transfer.id, confirmData)}
                   updating={updatingTask === transfer.id}
                   confirming={confirmingTask === transfer.id}
                 />
